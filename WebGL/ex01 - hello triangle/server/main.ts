8b1582f943969e31af91378acda422bdc32eaba2
Björn Kihlberg <bjorn.kihlberg@antura.se>
2017-05-12 10:46:14 +0200

    create project infrastructure for app resources

--- WebGL/ex01 - hello triangle/server/main.ts
+++ WebGL/ex01 - hello triangle/server/main.ts
@@ -5,5 +5,6 @@ const PORT = 8000
 
 app.use(express.static('./client/html'))
 app.use(express.static('./client/bin'))
+app.use(express.static('./client/resources'))
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
