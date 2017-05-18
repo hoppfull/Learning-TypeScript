5146b2eb3336a92a71e47b8534828c029f97ac35
Björn Kihlberg <bjorn.kihlberg@antura.se>
2017-05-18 11:59:04 +0200

    setup basic server

---
+++ Frameworks/Redux/server/main.ts
@@ -0,0 +1,9 @@
+import * as express from 'express'
+
+const app = express()
+const PORT = 8000
+
+app.use(express.static('./client/html'))
+app.use(express.static('./client/bin'))
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
